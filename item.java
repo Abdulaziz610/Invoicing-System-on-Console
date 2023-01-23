6bd96f293fd63dfd24c9ebb1ddbe5a49ce424bad
Abdulaziz610 <abdulazizalnasseri1998@gmail.com>
2023-01-22 23:49:25 -0800

    Update on class item
    Change the variable type to double,
    adding getter and setter,
    adding output invoice Format

--- item.java
+++ item.java
@@ -2,11 +2,77 @@ package invoicingSystem;
 
 public class item {
 	
-	Integer itemId;
-	Integer unitPrice;
-	Integer quantity;
-	Integer qtyAmountPerPrice;
+	private String itemName;
 	
-	String itemName;
-
+	private Integer itemId;
+	private Integer quantity;
+	private double unitPrice;
+	private double qtyAmountPerPrice;
+	
+	public String getItemName() {
+		return itemName;
+	}
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+	
+	
+	public Integer getItemId() {
+		return itemId;
+	}
+	public void setItemId(Integer itemId) {
+		this.itemId = itemId;
+	}
+	
+	
+	public Integer getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+	
+	
+	public double getUnitPrice() {
+		return unitPrice;
+	}
+	public void setUnitPrice(double unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+	
+	
+	public double getQtyAmountPerPrice() {
+		return qtyAmountPerPrice;
+	}
+	public void setQtyAmountPerPrice(double qtyAmountPerPrice) {
+		this.qtyAmountPerPrice = qtyAmountPerPrice;
+	}
+	
+	
+	
+	
+	
+	public static void outputFormat() {
+		System.out.println("=========================================================");
+		System.out.println("item Name: ");
+		System.out.println("item ID: ");
+		System.out.println("Quantity: ");
+		System.out.println("item Price: ");
+		System.out.println("Quantity Amount/Price: ");
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
